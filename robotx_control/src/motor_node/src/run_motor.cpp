1bc6ce61852df580a59f9dd22f24826320d16b1b
reinaldomaslim <rmaslim001@e.ntu.edu.sg>
2016-09-23 17:21:43 +0800

    modified:   robotx_control/CMakeLists.txt 	modified:   robotx_control/package.xml 	new file:   robotx_control/src/motor_node/CMakeLists.txt 	new file:   robotx_control/src/motor_node/package.xml 	new file:   robotx_control/src/motor_node/src/.run_motor.cpp.swp 	new file:   robotx_control/src/motor_node/src/run_motor.cpp 	new file:   robotx_control/src/motor_node/start_motor.launch

---
+++ robotx_control/src/motor_node/src/run_motor.cpp
@@ -0,0 +1,72 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Vector3.h"
+#include "geometry_msgs/Twist.h"
+
+double linear, angular;
+
+void cmdCallback(const geometry_msgs::Twist& msg)
+{
+  linear=msg.linear.x;
+  angular=msg.angular.z;
+}   
+
+void speedCallback(const geometry_msgs::Vector3& msg)
+{
+  ROS_INFO("right: %f , left: %f", msg.x, msg.y);
+}
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "start_motor");
+  ros::NodeHandle nh;
+
+  //publish (forward, turn, 0) range -500 to 500, for arduino
+  ros::Publisher motor_pub = nh.advertise<geometry_msgs::Vector3>("/motor_val", 1000); 
+  //subscribe to cmd_vel command (currently from turtlebot_teleop key)
+  ros::Subscriber cmd_sub= nh.subscribe("/cmd_vel_mux/input/teleop", 1000, cmdCallback);
+  //subscribe to 
+  ros::Subscriber speed_sub = nh.subscribe("/chatter", 1000, speedCallback);
+  
+  int forward_ratio, angular_ratio;
+  geometry_msgs::Vector3 motor_val;
+
+  ros::Rate loop_rate(50);
+
+  while(ros::ok())
+  {
+    nh.getParam("/motor_param/forward", forward_ratio); //values 0-2500 if linear 0.2
+    nh.getParam("/motor_param/angular", angular_ratio); //values 0-500 if angular 1
+   
+    motor_val.x=forward_ratio*linear;
+    motor_val.y=-1*angular_ratio*angular;
+       
+    if (motor_val.x>500)
+    {  
+      motor_val.x=500;
+    }
+    else if (motor_val.x<-500)
+    {
+      motor_val.x=-500;
+    }
+
+    if (motor_val.y>500)
+    {  
+      motor_val.y=500;
+    }
+    else if (motor_val.y<-500)
+    {
+      motor_val.y=-500;
+    }
+
+    motor_pub.publish(motor_val);
+    ros::spinOnce();
+
+    loop_rate.sleep();
+   }
+
+  return 0;
+
+}
+
+
