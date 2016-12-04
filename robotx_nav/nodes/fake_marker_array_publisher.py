b4f267e965b90ac5b360274261748c5cbada4453
renye1404 <renye.china@gmail.com>
2016-12-04 22:21:50 +0800

    fix marker array pub

--- robotx_nav/nodes/fake_marker_array_publisher.py
+++ robotx_nav/nodes/fake_marker_array_publisher.py
@@ -27,26 +27,24 @@ class MarkerArrayPublisher():
         rospy.init_node('fake_markerarray_pub', anonymous=False)
         r = rospy.Rate(1)
 
-        markerArray = MarkerArray()
-
         count = 1
-        MARKERS_MAX = 12
+        MARKERS_MAX = 4
         start_red_x, start_red_y = -5, 10
         start_green_x, start_green_y = 5, 10
         end_red_x, end_red_y = -5, 40
         end_green_x, end_green_y = 5, 40
         markerArray = MarkerArray()
-        marker = Marker()
-        marker.header.frame_id = "/map"
-        marker.scale.x = 0.2
-        marker.scale.y = 0.2
-        marker.scale.z = 0.2
-        marker.color.a = 1.0
-        marker.color.r = 1.0
-        marker.color.g = 1.0
-        marker.color.b = 0.0
 
         while not rospy.is_shutdown():
+            marker = Marker()
+            marker.header.frame_id = "/map"
+            marker.scale.x = 0.2
+            marker.scale.y = 0.2
+            marker.scale.z = 0.2
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
             # create start red totem
             marker.header.stamp = rospy.Time.now()
             marker.type = marker.CYLINDER  # totem
@@ -56,25 +54,29 @@ class MarkerArrayPublisher():
                 marker.id = 0  # red
                 marker.pose.position.x = start_red_x + self.random_noise()
                 marker.pose.position.y = start_red_y + self.random_noise()
+                # markerArray.markers[0] = marker
             elif count % 4 == 2: # start green
                 marker.id = 1  # green
                 marker.pose.position.x = start_green_x + self.random_noise()
                 marker.pose.position.y = start_green_y + self.random_noise()
+                # markerArray.markers[1] = marker
             elif count % 4 == 3: # end red
                 marker.id = 0  # red
                 marker.pose.position.x = end_red_x + self.random_noise()
                 marker.pose.position.y = end_red_y + self.random_noise()
+                # markerArray.markers[2] = marker
             else: # end green
                 marker.id = 1  # green
                 marker.pose.position.x = end_green_x + self.random_noise()
                 marker.pose.position.y = end_green_y + self.random_noise()
-            marker.pose.position.z = 0
+                marker.pose.position.z = 0
+                # markerArray.markers[3] = marker
             markerArray.markers.append(marker)
 
             # We add the new marker to the MarkerArray, removing the oldest
             # marker from it when necessary
             if(count > MARKERS_MAX):
-               markerArray.markers.pop(0)
+                markerArray.markers.pop(0)
 
             # Publish the MarkerArray
             pub.publish(markerArray)
