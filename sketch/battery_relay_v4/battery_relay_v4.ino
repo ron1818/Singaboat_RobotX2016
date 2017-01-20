da990ebc58afefce8c7771fb65216163bbe0450e
Ren Ye <renye.china@gmail.com>
2017-01-20 11:44:50 +0800

    add arduino for battery relay and thruster driver
    version 4, both use rosserial

---
+++ sketch/battery_relay_v4/battery_relay_v4.ino
@@ -0,0 +1,282 @@
+/* ren ye
+   2016-10-12
+   rosserial for battery relay and temperature water cooler
+   2016-10-20
+   rosserial for cmd_vel; mux with tr324
+   2017-01-13
+   rosserial for battery relay, led indicator and e-stop
+   2017-01-20
+   separate physical e-stop and remote e-stop
+
+   Connections:
+
+   Arduino   <=> B relay1
+   V12       <-> A1
+   D5        <-> B1
+   ~~~~~~~~~~<=> B relay2
+   V12       <-> A2
+   D6        <-> B2
+   ~~~~~~~~~~<=> E relay
+   D12       <-> Esense
+   D2        <-> EB
+   ~~~~~~~~~~<=> Battery sense
+   A0        <-> Bsense1
+   A1        <-> Bsense2
+   ~~~~~~~~~~<=> LED shield
+   Vin       <-> +ve
+   D10       <-> Red
+   D8        <-> Yellow
+   D7        <-> Green
+   ~~~~~~~~~~<=> Mega (for LED control signal)
+   D4        <-> LED_msb
+   D3        <-> LED_lsb
+
+
+
+
+*/
+
+#include <ros.h>
+#include <Arduino.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/MultiArrayDimension.h>
+#include <std_msgs/Float32MultiArray.h>
+
+// set up ros nodes and publishers
+std_msgs::Float32MultiArray control_msg;
+ros::Publisher pub("control", &control_msg);
+ros::NodeHandle nh;
+
+// initialize constants for battery relay and e stop relay
+const float BR0[2] = {200.0, 100.0}; // voltage divider for battery 1
+const float BR1[2] = {200.0, 100.0}; // voltage divider for battery 2
+const float Vmin[2] = {11.4, 11.4}; // cutoff voltage for battery
+const float ER[2] = {200.0, 100.0}; // voltage divider for e stop
+const int DT = 1000; // 1 second
+
+// digital pins
+#define BRelay1 5  // battery1 relay switch, active high
+#define BRelay2 6  // battery2 relay switch, active high
+#define ERelay 2   // e stop relay switch, active high
+#define LED_red 10 // led indicator red, active low
+#define LED_yellow 8 // led indicator yellow, active low
+#define LED_green 7 // led indicator gree, active low
+#define LED_ctrl1 4 // led indicator control MSb
+#define LED_ctrl0 3 // led indicator control LSb
+
+// analog pins
+#define Esense 12 // e stop voltage sense, HI~Open LO~Close
+#define Bsense1 A1 // battery1 voltage sense
+#define Bsense2 A0 // battery2 voltage sense
+
+void setup()
+{
+  pinMode(BRelay1, OUTPUT);
+  pinMode(BRelay2, OUTPUT);
+  pinMode(ERelay, OUTPUT);
+  pinMode(LED_red, OUTPUT);
+  pinMode(LED_yellow, OUTPUT);
+  pinMode(LED_green, OUTPUT);
+  pinMode(LED_ctrl1, INPUT);
+  pinMode(LED_ctrl0, INPUT);
+  pinMode(Esense, INPUT);
+  Serial.begin(57600);
+  // reset led
+  digitalWrite(LED_red, HIGH);
+  digitalWrite(LED_yellow, HIGH);
+  digitalWrite(LED_green, HIGH);
+  delay(1000);
+  digitalWrite(LED_red, LOW);
+  digitalWrite(LED_yellow, LOW);
+  digitalWrite(LED_green, LOW);
+  // init battery relay
+  digitalWrite(BRelay1, LOW);
+  digitalWrite(BRelay2, LOW);
+  // init e-stop relay
+  digitalWrite(ERelay, LOW);
+  // delay(1000);
+  // digitalWrite(ERelay, HIGH);
+
+  // init rosserial node and msg
+  nh.initNode();
+  control_msg.layout.dim = (std_msgs::MultiArrayDimension *)
+                           malloc(sizeof(std_msgs::MultiArrayDimension) * 1);
+  control_msg.layout.dim->label = "arduino_reading";
+  control_msg.layout.dim->size = 6;
+  // batt_msg.layout.dim_length = 1;
+  control_msg.layout.data_offset = 0;
+  control_msg.data = (float *)malloc(sizeof(float) * 5);
+  control_msg.data_length = 6;
+
+  nh.advertise(pub);
+  delay(2000);
+}
+
+long action_timer;
+long publisher_timer;
+
+void loop() {
+  float batt_v[2];
+  int m_e_stop;
+  float temp;
+  int batt_status = 0;
+  int led_status = 0;
+  int led_read[2];
+
+  // read physical e stop button data
+  m_e_stop = digitalRead(Esense);
+  // read led control data
+  led_read[1] = digitalRead(LED_ctrl1);
+  led_read[0] = digitalRead(LED_ctrl0);
+  /*Serial.print("e stop sense ");
+  Serial.print(batt_v[2]);
+  Serial.print(", led ");
+  Serial.print(digitalRead(LED_ctrl1));
+  Serial.println(digitalRead(LED_ctrl0));*/
+  // check and switch battery relay
+  if (m_e_stop == LOW) { // stop mode by physical e-stop
+    // Serial.println("case0");
+    // E stop is engaged, power to thrusters cut off
+    led_status = 0;
+    digitalWrite(LED_red, HIGH ); // turn on red light only
+    digitalWrite(LED_yellow, LOW);
+    digitalWrite(LED_green, LOW);
+    digitalWrite(ERelay, LOW);
+  } else {
+    if (led_read[1] == HIGH && led_read[0] == HIGH) { // stop mode by remote e-stop
+      // Serial.println("case1");
+      // E stop is engaged, power to thrusters cut off
+      led_status = 0;
+      digitalWrite(LED_red, HIGH); // turn on red light only
+      digitalWrite(LED_yellow, LOW);
+      digitalWrite(LED_green, LOW);
+      digitalWrite(ERelay, LOW);  // open the relay
+    } else if (led_read[1] == HIGH && led_read[0] == LOW) { //manual mode
+      // Serial.println("case2");
+      if (m_e_stop == LOW) { // stop mode by physical e-stop
+        // Serial.println("case21");
+        // E stop is engaged, power to thrusters cut off
+        led_status = 0;
+        digitalWrite(LED_red, HIGH); // turn on red light only
+        digitalWrite(LED_yellow, LOW);
+        digitalWrite(LED_green, LOW);
+        digitalWrite(ERelay, LOW);
+      } else {
+        // Serial.println("case22");
+        led_status = 1;
+        digitalWrite(LED_red, LOW);
+        digitalWrite(LED_yellow, HIGH);  // turn on yellow light only
+        digitalWrite(LED_green, LOW);
+        digitalWrite(ERelay, HIGH);
+      }
+    } else if (led_read[1] == LOW && led_read[0] == HIGH ) {  // automode
+      // Serial.println("case3");
+      if (m_e_stop == LOW) { // stop mode by physical e-stop
+        // Serial.println("case31");
+        // E stop is engaged, power to thrusters cut off
+        led_status = 0;
+        digitalWrite(LED_red, HIGH); // turn on red light only
+        digitalWrite(LED_yellow, LOW);
+        digitalWrite(LED_green, LOW);
+        digitalWrite(ERelay, LOW);
+      } else {
+        // Serial.println("case32");
+        led_status = 2;
+        digitalWrite(LED_red, LOW);
+        digitalWrite(LED_yellow, LOW);
+        digitalWrite(LED_green, HIGH); // turn on green light only
+        digitalWrite(ERelay, HIGH);
+      }
+    } else { // RC connection lost
+      // Serial.println("case4");
+      if (m_e_stop == LOW) { // stop mode by physical e-stop
+        // Serial.println("case41");
+        // E stop is engaged, power to thrusters cut off
+        led_status = 0;
+        digitalWrite(LED_red, HIGH); // turn on red light only
+        digitalWrite(LED_yellow, LOW);
+        digitalWrite(LED_green, LOW);
+        digitalWrite(ERelay, LOW);
+      } else {
+        // Serial.println("case42");
+        led_status = 3;
+        digitalWrite(LED_red, LOW);
+        digitalWrite(LED_yellow, HIGH); // turn on both yellow and green
+        digitalWrite(LED_green, HIGH); 
+        digitalWrite(ERelay, HIGH);
+      }
+    }
+  }
+  // read voltage divider data
+  batt_v[0] = read_voltage(Bsense1);
+  batt_v[1] = read_voltage(Bsense2);
+  Serial.print("batt sense ");
+  Serial.print(batt_v[0]);
+  Serial.print(", ");
+  Serial.println(batt_v[1]);
+  Serial.print("relay status ");
+ 
+  Serial.println(batt_v[0] - Vmin[0]);
+  Serial.println(batt_v[1] - Vmin[1]);
+  
+  // check and switch battery relay
+  if (batt_v[0] >= Vmin[0] && batt_v[1] >= Vmin[1]) {
+    batt_status = 0;  // Batt1 healthy Batt2 healthy
+     Serial.println(batt_status);
+    digitalWrite(BRelay1, LOW);
+    digitalWrite(BRelay2, LOW);
+  } else if (batt_v[0] >= Vmin[0] && batt_v[1] < Vmin[1]) {
+    batt_status = 1; // Batt1 healthy Batt2 unhealthy
+     Serial.println(batt_status);
+    digitalWrite(BRelay1, LOW);
+    digitalWrite(BRelay2, HIGH);
+  } else if (batt_v[0] < Vmin[0] && batt_v[1] >= Vmin[1]) {
+    batt_status = 2; // Batt1 unhealthy Batt2 healthy
+     Serial.println(batt_status);
+    digitalWrite(BRelay1, HIGH);
+    digitalWrite(BRelay2, LOW);
+  } else {
+    batt_status = 3; // Batt1 unhealthy Batt2 unhealthy
+     Serial.println(batt_status);
+    digitalWrite(BRelay1, LOW);
+    digitalWrite(BRelay2, LOW);
+    // blink once
+    digitalWrite(LED_red, LOW);
+    digitalWrite(LED_yellow, LOW);
+    digitalWrite(LED_green, LOW);
+    delay(1000);
+    digitalWrite(LED_red, HIGH);
+    digitalWrite(LED_yellow, HIGH);
+    digitalWrite(LED_green, HIGH);
+    delay(1000);
+  }
+
+  delay(100);
+
+  // publish topic
+  if (millis() > publisher_timer) {
+
+    // publish battery and temperature
+    control_msg.data[0] = batt_v[0];
+    control_msg.data[1] = batt_v[1];
+    control_msg.data[2] = batt_v[2];
+    control_msg.data[3] = batt_status;
+    control_msg.data[4] = led_status;
+    control_msg.data[5] = 0; // reserved
+    pub.publish(&control_msg);
+
+    publisher_timer = millis() + DT;
+
+  }
+  nh.spinOnce();
+}
+
+float read_voltage(int id) {
+  if (id == 1) {
+    return (BR0[0] + BR0[1]) / BR0[1] * analogRead(id) * 5.0 / 1024;
+  } else if (id == 0) {
+    return (BR1[0] + BR1[1]) / BR1[1] * analogRead(id) * 5.0 / 1024;
+  } else { // id == 2, e stop
+    return (ER[0] + ER[1]) / ER[1] * analogRead(id) * 5.0 / 1024;
+  }
+}
---
+++ sketch/thruster_driver_v4/thruster_driver_v4.ino
@@ -0,0 +1,223 @@
+/*  motor rudder controller to differential drive
+    ren ye
+    2016-08-11
+    init
+    2016-11-10
+    use RC controller and pwm generator to control
+    use Serial library
+    2017-01-13
+    use flysky to control
+    use mega
+    send e-stop led status
+
+    flysky ia6
+    ia6     Arduino
+    Ch1   ->  D7
+    Ch2   ->  D8
+    Ch5   ->  D9 (ack) pwm input
+
+
+    Arduino   74157
+    D4    ->  ~sel onoff output
+    D5    ->  B_1
+    D6    ->  B_2
+    V5    ->  VCC
+    GND   ->  GND
+*/
+#include <ros.h>
+#include <Arduino.h>
+#include <std_msgs/MultiArrayDimension.h>
+#include <std_msgs/UInt16MultiArray.h>
+#include <Servo.h>
+
+ros::NodeHandle nh;
+
+Servo left_rc;
+Servo right_rc;
+Servo left_ros;
+Servo right_ros;
+
+#define SteeringPin 36  // rudder
+#define ThrottlePin 38  // propeller
+#define CH5 42   // mode select pin
+#define SEL 44   // mux select pin
+#define left_thrusterRC 5 // left thruster rc
+#define right_thrusterRC 7   // right thruster rc
+#define left_thrusterROS 6 // left thruster rc
+#define right_thrusterROS 8   // right thruster rc
+#define LED_ctrl1 46
+#define LED_ctrl0 48
+
+float timer_period;
+unsigned int T, T_reset, T_prev, T_offset, T_min, T_max;
+
+const int max_pwm = 1900;
+const int min_pwm = 1100;
+const int ack_threshold[4] = {900, 1200, 1600, 2000};
+const int gap = 10;
+const int neutral_pwm = 1500;
+const float wheel_sep = 0.6;
+const float DT = 0.02;
+
+int linear_x_center = neutral_pwm;
+int angular_z = neutral_pwm;
+
+int left_pwmRC = neutral_pwm;
+int right_pwmRC = neutral_pwm;
+int left_pwmROS = neutral_pwm;
+int right_pwmROS = neutral_pwm;
+
+// pwm callback
+void pwmCb(const std_msgs::UInt16MultiArray& msg) {
+  // read pwm and convert to pwm
+  left_pwmROS = msg.data[0];
+  right_pwmROS = msg.data[1];
+}
+
+ros::Subscriber<std_msgs::UInt16MultiArray> pwm_sub("/pwm", &pwmCb );
+
+void setup() {
+  pinMode(SteeringPin, INPUT);
+  pinMode(ThrottlePin, INPUT);
+  pinMode(CH5, INPUT);
+  pinMode(left_thrusterROS, OUTPUT);
+  pinMode(right_thrusterROS, OUTPUT);
+  pinMode(left_thrusterRC, OUTPUT);
+  pinMode(right_thrusterRC, OUTPUT);
+  pinMode(SEL, OUTPUT);
+  pinMode(LED_ctrl1, OUTPUT);
+  pinMode(LED_ctrl0, OUTPUT);
+
+  digitalWrite(LED_ctrl1, LOW);
+  digitalWrite(LED_ctrl0, LOW);
+  digitalWrite(SEL, LOW);  // mux RC
+
+  // Serial.begin(57600);
+  
+  // attach esc
+  left_rc.attach(left_thrusterRC);
+  right_rc.attach(right_thrusterRC);
+  left_ros.attach(left_thrusterROS);
+  right_ros.attach(right_thrusterROS);
+
+
+  left_rc.writeMicroseconds(neutral_pwm);
+  right_rc.writeMicroseconds(neutral_pwm);
+  left_ros.writeMicroseconds(neutral_pwm);
+  right_ros.writeMicroseconds(neutral_pwm);
+  delay(1000);
+
+  nh.initNode();
+  nh.subscribe(pwm_sub);
+  delay(1000);
+
+}
+
+long action_timer;
+long publisher_timer;
+
+void loop() {
+  int sel = 1;
+  // read AckInPin check if it has pwm input
+  int mode_pwm = pulseIn(CH5, HIGH);
+
+
+  /* Serial.print(mode_pwm);
+  Serial.print(", "); */
+  if (mode_pwm < (ack_threshold[0] - gap)) { // RC not connected
+    sel = -1;
+    digitalWrite(LED_ctrl1, LOW); //
+    digitalWrite(LED_ctrl0, LOW); //
+    digitalWrite(SEL, LOW);  // mux auto
+    left_ros.writeMicroseconds(left_pwmROS);
+    right_ros.writeMicroseconds(right_pwmROS);
+  } else if (mode_pwm >= (ack_threshold[0] + gap) && mode_pwm <= (ack_threshold[1] - gap)) { // e-stop when <1300
+    sel = 0;
+    digitalWrite(LED_ctrl1, HIGH); // e-stop relay on
+    digitalWrite(LED_ctrl0, HIGH); //
+    digitalWrite(SEL, HIGH);  // mux ROS
+    left_rc.writeMicroseconds(neutral_pwm);  // put neutral in case
+    right_rc.writeMicroseconds(neutral_pwm);
+  } else if (mode_pwm >= (ack_threshold[1] + gap) && mode_pwm <= (ack_threshold[2] - gap)) { // manual when ~1500
+    sel = 1;
+    digitalWrite(LED_ctrl1, HIGH); // e-stop relay on
+    digitalWrite(LED_ctrl0, LOW); //
+    digitalWrite(SEL, LOW);  // mux RC
+    int tmp_angular_z = pulseIn(SteeringPin, HIGH);
+    int tmp_linear_x_center = pulseIn(ThrottlePin, HIGH);
+
+    if (tmp_linear_x_center > 500) {
+      linear_x_center = tmp_linear_x_center;
+    }
+    if (tmp_angular_z > 500) {
+      angular_z = tmp_angular_z;
+    }
+    /* Serial.print(linear_x_center);
+      Serial.print(", ");
+      Serial.println(angular_z);*/
+
+    conv_differential(linear_x_center, angular_z);
+    // write to pwm motor pin
+    /* Serial.print(left_pwm);
+      Serial.print(", ");
+      Serial.println(right_pwm); */
+    left_rc.writeMicroseconds(left_pwmRC);
+    right_rc.writeMicroseconds(right_pwmRC);
+  } else {  // auto
+    digitalWrite(SEL, HIGH);  // mux ROS
+    digitalWrite(LED_ctrl1, LOW); // e-stop relay on
+    digitalWrite(LED_ctrl0, HIGH); //
+    sel = 0;
+    left_ros.writeMicroseconds(left_pwmROS);
+    right_ros.writeMicroseconds(right_pwmROS);
+
+    // Serial.println("from PCA9685");
+    // 74ls157 will take pwm from pca9685
+  }
+  delay(50);
+
+  /* // publish topic
+    if (millis() > publisher_timer) {
+
+    // publish battery and temperature
+    control_msg.data[0] = left_pwm;
+    control_msg.data[1] = right_pwm;
+    control_msg.data[2] = left_pwm_ros;
+    control_msg.data[3] = right_pwm_ros;
+    control_msg.data[4] = sel;
+    pub.publish(&control_msg);
+
+    publisher_timer = millis() + DT;
+
+    } */
+  nh.spinOnce();
+
+}
+
+void conv_differential(int linear_x_center, int angular_z) {
+  int insensitive_value = 50;
+  if (linear_x_center > (neutral_pwm + insensitive_value) ||
+      angular_z > (neutral_pwm + insensitive_value) ||
+      linear_x_center < (neutral_pwm - insensitive_value) ||
+      angular_z < (neutral_pwm - insensitive_value)) {
+  }
+
+  int angular_offset = insensitive(angular_z) - neutral_pwm;
+  left_pwmRC = linear_x_center - angular_offset;
+  right_pwmRC = linear_x_center + angular_offset;
+  left_pwmRC = insensitive(left_pwmRC);
+  right_pwmRC = insensitive(right_pwmRC);
+
+}
+
+int insensitive(int pwm) {
+  int insensitive_value = 50;
+  pwm = constrain(pwm, min_pwm, max_pwm);
+
+  if (pwm < (neutral_pwm + insensitive_value) &&
+      pwm > (neutral_pwm - insensitive_value)) {
+    return neutral_pwm;
+  } else {
+    return pwm;
+  }
+}
